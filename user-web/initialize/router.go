4d9aa5c0f3e378bcb7263583a4a75d97922bc2ed
chenjie <657499186@qq.com>
2022-06-07 00:51:38 +0800

    跨域

--- user-web/initialize/router.go
+++ user-web/initialize/router.go
@@ -2,11 +2,14 @@ package initialize
 
 import (
 	"github.com/gin-gonic/gin"
+	"shop-api/user-web/middlewares"
 	"shop-api/user-web/router"
 )
 
 func InitRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(middlewares.Cors())
 
 	group := r.Group("/v1")
 
