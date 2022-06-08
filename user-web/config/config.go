e7f15ba7dbb5040b86dac732dd4df2cf96cb49fe
chenjie <657499186@qq.com>
2022-06-08 22:58:35 +0800

    阿里云短信

--- user-web/config/config.go
+++ user-web/config/config.go
@@ -5,7 +5,20 @@ type ServerConfig struct {
 	Port        int           `mapstructure:"port"`
 	UserSrvInfo UserSrvConfig `mapstructure:"userSrv"`
 	JwtInfo     JWTConfig     `mapstructure:"jwt"`
+	AliSmsInfo  AliSmsConfig  `mapstructure:"sms" json:"sms"`
+	RedisInfo   RedisConfig   `mapstructure:"redis" json:"redis"`
 	ConsulInfo  ConsulConfig  `mapstructure:"consul"`
+}
+
+type AliSmsConfig struct {
+	ApiKey     string `mapstructure:"key" json:"key"`
+	ApiSecrect string `mapstructure:"secrect" json:"secrect"`
+}
+
+type RedisConfig struct {
+	Host   string `mapstructure:"host" json:"host"`
+	Port   int    `mapstructure:"port" json:"port"`
+	Expire int    `mapstructure:"expire" json:"expire"`
 }
 
 type UserSrvConfig struct {
