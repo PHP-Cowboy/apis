de8b4412e8ecaf00e2e412fd175955548235cf07
chenjie <657499186@qq.com>
2022-06-14 15:02:14 +0800

    启动 协程

--- goods-web/main.go
+++ goods-web/main.go
@@ -40,9 +40,13 @@ func main() {
 	}
 
 	zap.S().Info("服务启动中,端口:", serverConfig.Port)
-	if err := g.Run(fmt.Sprintf(":%d", serverConfig.Port)); err != nil {
-		zap.S().Panicf("启动失败:", err.Error())
-	}
+
+	go func() {
+		err := g.Run(fmt.Sprintf(":%d", serverConfig.Port))
+		if err != nil {
+			zap.S().Panicf("启动失败:", err.Error())
+		}
+	}()
 
 	//接收终止信号
 	quit := make(chan os.Signal)
